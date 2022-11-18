ea5fa2e8c14971d0dccbf59cb3d66e3adf78302e
Eva Gonzalez <eva.gonzalez@ingka.ikea.com>
2022-11-18 08:55:56 +0100

    organize folders and config styles pages

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Box, Typography } from '@mui/material';
+import { LoginForm } from './pages';
+import './App.css';
+
+function App() {
+  return (
+    <Box
+      className='App'
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      flexDirection='column'
+    >
+      <Typography variant='h2' mb='20px' textAlign='center'>
+        React Testing Library & Jest
+      </Typography>
+      <LoginForm />
+    </Box>
+  );
+}
+
+export default App;

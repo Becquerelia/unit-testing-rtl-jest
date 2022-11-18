a3a2673301fa3af61ac0562debe2aa130d4daba9
Eva Gonzalez <eva.gonzalez@ingka.ikea.com>
2022-11-18 16:41:12 +0100

    create mock for testing

---
+++ src/_mock_/LoginMock.jsx
@@ -0,0 +1,9 @@
+export const LoginFormMock = {
+    username: 'Pikachu',
+    password: 'Meatball123?'    
+}
+
+export const LoginFormMockError = {
+    username: 'pikapikapikapikapikapika',
+    password: 'Meatball123'    
+}
--- src/_test_/LoginForm.test.jsx
+++ src/_test_/LoginForm.test.jsx
@@ -1,5 +1,7 @@
-import { render, cleanup, screen } from '@testing-library/react';
+import { render, cleanup, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { LoginForm } from '../pages/LoginForm';
+import { LoginFormMock } from '../_mock_/LoginMock';
 
 describe('LoginForm', () => {
     afterEach(cleanup);
@@ -9,7 +11,23 @@ describe('LoginForm', () => {
         render(<LoginForm />);
     });
 
-    it('should display two inputs', () => {
-        expect(screen.getByLabelText(/nombre de usuario/i)).toBeInTheDocument();
+    it('should display two inputs', async () => {
+        const usernameInput = screen.getByLabelText(/nombre de usuario/i);
+        const passwordInput = screen.getByLabelText(/contraseña/i);
+
+        expect(usernameInput).toBeInTheDocument();
+        expect(passwordInput).toBeInTheDocument();
+
+        expect(usernameInput).toHaveValue('');
+        expect(passwordInput).toHaveValue('');
+
+        await userEvent.type(usernameInput, LoginFormMock.username);
+        await userEvent.type(passwordInput, LoginFormMock.password);
+       
+
+        await waitFor(() => {
+            expect(usernameInput).toHaveValue(LoginFormMock.username);
+            expect(passwordInput).toHaveValue(LoginFormMock.password);
+        })
     });
 }); 

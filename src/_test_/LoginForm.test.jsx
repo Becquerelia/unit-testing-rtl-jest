3a7bc375dfea2f973f5b00b63fd28f7585e3cbd0
Eva Gonzalez <eva.gonzalez@ingka.ikea.com>
2022-11-22 10:17:43 +0100

    test introducing incorrect values in form from LoginFormMockError

--- src/_test_/LoginForm.test.jsx
+++ src/_test_/LoginForm.test.jsx
@@ -1,7 +1,8 @@
 import { render, cleanup, screen, waitFor, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import user from '@testing-library/user-event';
 import { LoginForm } from '../pages/LoginForm';
-import { LoginFormMock } from '../_mock_/LoginMock';
+import { LoginFormMock, LoginFormMockError } from '../_mock_/LoginMock';
 
 describe('LoginForm', () => {
     afterEach(cleanup);
@@ -37,9 +38,24 @@ describe('LoginForm', () => {
           expect(passwordInput).toHaveValue(LoginFormMock.password);                         
         });
 
-        //await waitFor(() => expect(submitButton).not.toBeDisabled());
+        //await waitFor(() => expect(submitButton).toBeDisabled(""));
+        // const handleClick = jest.fn()
+        // await user.click(submitButton);
+        // expect(handleClick).toHaveBeenCalledTimes(1)
+    });    
 
-    });
+    it ('should disable submit button if form values are incorrect', async () => {
+        const usernameInput = screen.getByLabelText(/nombre de usuario/i);
+        const passwordInput = screen.getByLabelText(/contraseña/i);
+        const submitButton = screen.getByRole('button', { name: /login/i });        
 
+        await userEvent.type(usernameInput, LoginFormMock.username);
+        await userEvent.type(passwordInput, LoginFormMock.password);
+
+        await waitFor(() => {
+          expect(usernameInput).toHaveValue(LoginFormMockError.username);
+          expect(passwordInput).toHaveValue(LoginFormMockError.password);                         
+        });
+    });    
     
 }); 
